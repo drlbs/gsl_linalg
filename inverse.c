f2baac7ad442c3fc9ef0ef1da0d6c94fcf290368
Andrew J. Pounds <pounds_aj@mercer.edu>
2017-11-25 09:17:27 -0500

    think inversion is working. need to test

--- inverse.c
+++ inverse.c
@@ -5,14 +5,13 @@
 int main (void)
 {
 
-  const int N = 20;
+  const int N = 2;
   int permutation_sign, i, j;  
   double diag_prod;
 
   // Create pointers and allocate space to hold the matrices
   gsl_matrix *a = gsl_matrix_alloc(N,N);
   gsl_matrix *b = gsl_matrix_alloc(N,N);
-  gsl_vector *x = gsl_vector_alloc(N);
 
   // Allocate space for the permutation matrix
   gsl_permutation * p = gsl_permutation_alloc (N);
@@ -22,35 +21,23 @@ int main (void)
       for ( j=1; j<=N; j++ )
           gsl_matrix_set( a, i-1, j-1, (double) 1 / ( (double) (i+j-1)) );
 
-  // Build the Identiry Matrix, store in b  
-  for ( i=0; i<N; i++ )
-      for ( j=0; j<N; j++ )
-          if ( i != j ) 
-             gsl_matrix_set( b, i, j, (double) 0 );
-          else
-             gsl_matrix_set( b, i, j, (double) 1 );
-
   // Decompose the matrix using the PLU method
   gsl_linalg_LU_decomp (a, p, &permutation_sign);
 
-  // Do the back substitution to solve x 
-  gsl_linalg_LU_solve (a, p, b, x);
+  // Do the inversion  
+  gsl_linalg_LU_invert (a, p, b);
 
-  // Now compute the product of the diagonal of the decomposed a matrix
-  diag_prod = (double) 1.0
-  for (i=0; i<N; i++ ) 
-      diag_prod = diag_prod * gsl_matrix_get(a, i, i);
+  // Now print 
+  printf("The inverse matrix\n");
+  for (i=0; i<N; i++ ) {
+      for (j=0; j<N; j++) { 
+         printf("\t%g", gsl_matrix_get(b,i,j));
+      }
+      printf("\n");
+  } 
 
-  // Now fix the sign based on row interchange data
-  diag_prod = diag_prod * permutation_sign;
-
-  gsl_vector_complex_fprintf(stdout, x, "%g");
-
-  printf ("The determinant of the matrix is = %f\n", diag_prod);
-
-  gsl_matrix_free(a_data);
-  gsl_matrix_free(b_data);
-  gsl_vector_free(x);
+  gsl_matrix_free(a);
+  gsl_matrix_free(b);
   gsl_permutation_free (p);
   return 0;
 }
